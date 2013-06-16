a45de2362b1bfc1cadb16dd17ca679c0c9d35ef3
wmthietit <nvthiet.khtn08@gmail.com>
2013-06-17 00:27:42 +0700

    update mypage

--- catalog/view/javascript/common.js
+++ catalog/view/javascript/common.js
@@ -19,6 +19,85 @@ function getURLVar(urlVarName) {
 	return urlVarValue;
 } 
 
+function SingleListFeed(el) {
+	this.horizontalElement = el.find('.has-horizontal');
+	this.mainContent = el.find('#y-main-content');
+	this.maxHeight = this.mainContent.height();
+	this.rootElement = el;
+	this.setSize();
+	this.makeScroll();
+}
+
+SingleListFeed.prototype.setSize = function() {
+	var feeds = this.mainContent.find('.feed');
+	feeds.each(function() {
+		$(this).attr('data-width', $(this).outerWidth()); 
+		$(this).attr('data-height', $(this).outerHeight() + parseInt($(this).css('margin-bottom'))); 
+	});
+}
+
+SingleListFeed.prototype.makeScroll = function(){
+	if(typeof this.horizontalElement == "undefined" || this.horizontalElement.length == 0){
+		return;
+	}
+	var firstEl = this.horizontalElement.first();
+	var feedSet = firstEl.find('.feed'); 
+	var width=0, height=0, totalHeight = 0, totalWidth=0,columnIndex=0, columnId, newColoumn;
+
+	//Remove all columns:
+	firstEl.find('.column').attr('id','').css('display','none'); 
+
+	for (var i = 0; i < feedSet.length; i++) {
+		width = parseInt($(feedSet[i]).attr('data-width'), 10);
+		height = parseInt($(feedSet[i]).attr('data-height'), 10);
+		if(totalHeight == 0){
+			columnId = 'column-' + columnIndex;
+			newColoumn = $("<div class='column' id='" + columnId + "'></div>");
+			newColoumn.width(width);
+			newColoumn.height('100%');
+			newColoumn.css('float','left');
+			newColoumn.css('margin-right','25px');
+			firstEl.append(newColoumn);
+			totalWidth += width + 27;
+		}
+		if(totalHeight + height >=this.maxHeight) { 
+			columnIndex ++;
+			totalHeight = height;
+			columnId = 'column-' + columnIndex;
+			newColoumn = $("<div class='column' id='" + columnId + "'></div>");
+			newColoumn.width(width);
+			newColoumn.height('100%');
+			newColoumn.css('float','left');
+			newColoumn.css('margin-right','25px');
+			firstEl.append(newColoumn);
+			totalWidth += width + 27;
+			$('#' + columnId).append(feedSet[i]);
+		}else{
+			totalHeight += height;			
+			$('#' + columnId).append(feedSet[i]);
+		}
+	}
+	this.mainContent.width(totalWidth);
+	this.rootElement.niceScroll();
+	this.resizeListFeed();
+}
+
+SingleListFeed.prototype.resizeListFeed = function() {
+	var that = this;
+	$(window).resize(function() {
+	    if(this.resizeTO) 
+	    	clearTimeout(this.resizeTO);
+	    this.resizeTO = setTimeout(function() {
+	        $(this).trigger('resizeEnd');
+	    }, 800);
+	});
+
+	$(window).bind('resizeEnd', function() {
+		that.makeScroll();		
+	});
+
+}
+
 jQuery.fn.makeScrollWithoutCalResize = function() {
 	$(this).niceScroll();	
 }
