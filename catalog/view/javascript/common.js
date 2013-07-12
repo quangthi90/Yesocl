12687fc0268baadd2caf256ac15ddbab099b9a16
wmthiet90 <nvthiet.khtn08@gmail.com>
2013-07-13 00:00:29 +0700

    update

--- catalog/view/javascript/common.js
+++ catalog/view/javascript/common.js
@@ -1,22 +1,3 @@
-function getURLVar(urlVarName) {
-	var urlHalves = String(document.location).toLowerCase().split('?');
-	var urlVarValue = '';
-	
-	if (urlHalves[1]) {
-		var urlVars = urlHalves[1].split('&');
-
-		for (var i = 0; i <= (urlVars.length); i++) {
-			if (urlVars[i]) {
-				var urlVarPair = urlVars[i].split('=');
-				
-				if (urlVarPair[0] && urlVarPair[0] == urlVarName.toLowerCase()) {
-					urlVarValue = urlVarPair[1];
-				}
-			}
-		}
-	}	
-	return urlVarValue;
-} 
 /*
 Quick access functions
 */
