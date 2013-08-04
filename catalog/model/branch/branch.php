1a99498077a62a69b1366e910993bf367eed9203
bommer <bommer@bommerdesign.com>
2013-08-05 01:57:30 +0700

    - Update get branch for home feed with status is true
    Signed-off-by: bommer <bommer@bommerdesign.com>

--- catalog/model/branch/branch.php
+++ catalog/model/branch/branch.php
@@ -1,7 +1,9 @@
 <?php
 class ModelBranchBranch extends Doctrine {
 	public function getAllBranchs(){
-		return $this->dm->getRepository('Document\Branch\Branch')->findAll();
+		return $this->dm->getRepository('Document\Branch\Branch')->findBy( array(
+			'status' => true
+		));
 	}
 
 	public function getBranch( $branch_id ){
