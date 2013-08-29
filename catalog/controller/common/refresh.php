33281505a8e7f6e5582047793591050b27238043
bommer <bommer@bommerdesign.com>
2013-08-29 18:06:47 +0700

    - Move all comment function out of post controller
    Signed-off-by: bommer <bommer@bommerdesign.com>

--- catalog/controller/common/refresh.php
+++ catalog/controller/common/refresh.php
@@ -55,7 +55,7 @@ class ControllerCommonRefresh extends Controller {
 			
 			$post['href_user'] = $this->url->link('account/edit', 'user_slug=' . $post['user']['slug'], 'SSL');
 			$post['href_post'] = $this->url->link('post/detail', 'post_slug=' . $post['slug'] . '&post_type=' . $this->config->get('common')['type']['branch'], 'SSL');
-			$post['href_status'] = $this->url->link('post/post/getComments', 'type_slug=' . $branch_slug, 'SSL');
+			$post['href_status'] = $this->url->link('post/comment/getComments', 'type_slug=' . $branch_slug, 'SSL');
 
 			$list_posts[] = $post;
 			
@@ -69,7 +69,7 @@ class ControllerCommonRefresh extends Controller {
 		
 		$this->data['date_format'] = $this->language->get('date_format_full');
 		$this->data['post_type'] = $this->config->get('common')['type']['branch'];
-		$this->data['action']['comment'] = $this->url->link('post/post/addComment', '', 'SSL');
+		$this->data['action']['comment'] = $this->url->link('post/comment/addComment', '', 'SSL');
 		
 		if (file_exists(DIR_TEMPLATE . $this->config->get('config_template') . '/template/common/refresh.tpl')) {
 			$this->template = $this->config->get('config_template') . '/template/common/refresh.tpl';
---
+++ catalog/controller/post/comment.php
@@ -0,0 +1,204 @@
+<?php 
+class ControllerPostComment extends Controller {
+	private $error = array();
+
+	public function addComment(){
+        if ( $this->customer->isLogged() ) {
+            $data['user_id'] = $this->customer->getId();
+        }else {
+            return $this->response->setOutput(json_encode(array(
+                'success' => 'not ok: user not login'
+            )));
+        }
+
+        if ( empty($this->request->post['post_slug']) ){
+            return $this->response->setOutput(json_encode(array(
+                'success' => 'not ok: post slug is empty'
+            )));
+        }
+
+        if ( empty($this->request->post['post_type']) ){
+            return $this->response->setOutput(json_encode(array(
+                'success' => 'not ok: post type is empty'
+            )));
+        }
+
+        if ( empty($this->request->post['content']) ){
+            return $this->response->setOutput(json_encode(array(
+                'success' => 'not ok: content is empty'
+            )));
+        }
+
+        if ( $this->validate() ) {
+            $data['post_slug'] = $this->request->post['post_slug'];
+            $data['content'] = $this->request->post['content'];
+            $data['post_type'] = $this->request->post['post_type'];
+        }else {
+            return $this->response->setOutput(json_encode(array(
+                'success' => 'not ok: validate false'
+            )));
+        }
+        
+        switch ($data['post_type']) {
+            case $this->config->get('post')['type']['branch']:
+                $this->load->model('branch/comment');
+                $comment = $this->model_branch_comment->addComment( $data );
+                break;
+            
+            default:
+                $comment = array();
+                break;
+        }
+        
+        if ( $comment == false ){
+            return $this->response->setOutput(json_encode(array(
+                'success' => 'not ok: add comment have error'
+            )));
+        }
+
+        $this->load->model('user/user');
+        $user = $this->model_user_user->getUser( $comment['user_slug'] );
+
+        $this->load->model('tool/image');
+        
+        if ( $user && $user['avatar'] && file_exists(DIR_IMAGE . $user['avatar']) ){
+            $avatar = $this->model_tool_image->resize( $user['avatar'], 180, 180 );
+        }elseif ( $user && $user['email'] ){
+            $avatar = $this->model_tool_image->getGavatar( $user['email'], 180 );
+        }else{
+            $avatar = $this->model_tool_image->getGavatar( $comment['email'], 180 );
+        }
+
+        if ( $user && $user['username'] ){
+            $comment['author'] = $user['username'];
+        }
+
+        $comment['avatar'] = $avatar;
+        $comment['href_user'] = $this->url->link('account/edit', 'user_slug=' . $user['slug'], 'SSL');
+        $comment['created'] = $comment['created']->format( $this->language->get('date_format_full') );
+
+        return $this->response->setOutput(json_encode(array(
+            'success' => 'ok',
+            'comment' => $comment
+        )));
+    }
+
+  	private function validate() {
+    	if ((utf8_strlen($this->request->post['content']) < 1)) {
+      		$this->error['content'] = $this->language->get('error_content');
+    	}
+		
+    	if (!$this->error) {
+      		return true;
+    	} else {
+      		return false;
+    	}
+  	}
+
+    public function getComments(){
+        $data = array();
+
+        if ( isset($this->request->post['post_slug']) && !empty($this->request->post['post_slug']) ){
+            $data['post_slug'] = $this->request->post['post_slug'];
+        }else{
+            return $this->response->setOutput(json_encode(array(
+                'success' => 'not ok: post slug empty'
+            )));
+        }
+
+        if ( isset($this->request->post['post_type']) && !empty($this->request->post['post_type']) ){
+            $data['post_type'] = $this->request->post['post_type'];
+        }else{
+            return $this->response->setOutput(json_encode(array(
+                'success' => 'not ok: post type empty'
+            )));
+        }
+
+        if ( isset($this->request->post['start']) && !empty($this->request->post['start']) ){
+            $data['start'] = $this->request->post['start'];
+        }
+
+        if ( isset($this->request->post['limit']) && !empty($this->request->post['limit']) ){
+            $data['limit'] = $this->request->post['limit'];
+        }
+        
+        switch ($data['post_type']) {
+            case $this->config->get('post')['type']['branch']:
+                $this->load->model('branch/comment');
+                $comments = $this->model_branch_comment->getComments( $data );
+                break;
+            
+            default:
+                $comments = array();
+                break;
+        }
+
+        $this->load->model('user/user');
+        $this->load->model('tool/image');
+
+        foreach ( $comments as $key => $comment ) {
+            $user = $this->model_user_user->getUser( $comment['user_slug'] );
+            
+            if ( $user && $user['avatar'] && file_exists(DIR_IMAGE . $user['avatar']) ){
+                $avatar = $this->model_tool_image->resize( $user['avatar'], 180, 180 );
+            }elseif ( $user && $user['email'] ){
+                $avatar = $this->model_tool_image->getGavatar( $user['email'], 180 );
+            }else{
+                $avatar = $this->model_tool_image->getGavatar( $comment['email'], 180 );
+            }
+
+            if ( $user && $user['username'] ){
+                $comments[$key]['author'] = $user['username'];
+            }else{
+                $comments[$key]['author'] = $comment['author'];
+            }
+
+            $comments[$key]['avatar'] = $avatar;
+            $comments[$key]['href_user'] = $this->url->link('account/edit', 'user_slug=' . $user['slug'], 'SSL');
+            $comments[$key]['created'] = $comment['created']->format( $this->language->get('date_format_full') );
+        }
+
+        return $this->response->setOutput(json_encode(array(
+            'success' => 'ok',
+            'comments' => empty($comments) ? array() : $comments
+        )));
+    }
+
+    public function like(){
+        $data = array();
+
+        if ( !empty($this->request->post['post_slug']) ){
+            $data['post_slug'] = $this->request->post['post_slug'];
+        }else{
+            return $this->response->setOutput(json_encode(array(
+                'success' => 'not ok: post slug empty'
+            )));
+        }
+
+        if ( !empty($this->request->post['post_type']) ){
+            $data['post_type'] = $this->request->post['post_type'];
+        }else{
+            return $this->response->setOutput(json_encode(array(
+                'success' => 'not ok: post type empty'
+            )));
+        }
+
+        $data['likerId'] = $this->customer->getId();
+        
+        switch ($data['post_type']) {
+            case $this->config->get('post')['type']['branch']:
+                $this->load->model('branch/post');
+                $post = $this->model_branch_post->editPost( $data['post_slug'], $data );
+                break;
+            
+            default:
+                break;
+        }
+
+        return $this->response->setOutput(json_encode(array(
+            'success' => 'ok',
+            'like_count' => count($post->getLikerIds())
+        )));
+    }
+}
+?>
