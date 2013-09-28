e0b16b5b3dfe52408434dd24e180780837c8b79f
vn3352 <vn3352@bommerdesign.com>
2013-09-28 20:50:47 +0700

    - Update for user change avatar.
    Signed-off-by: vn3352 <vn3352@bommerdesign.com>

---
+++ catalog/language/english/account/avatar.php
@@ -0,0 +1,14 @@
+<?php
+// Heading 
+$_['heading_title']  = 'Change Avatar';
+
+// Text
+$_['text_select_image']   = 'Select image';
+$_['text_change']   = 'Change';
+$_['text_remove']   = 'Remove';
+$_['text_success']   = 'Success: Your avatar has been successfully updated.';
+
+// Entry
+
+// Error
+?>
