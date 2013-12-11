b879f0adb90ff292bc5c23f86bb28cea812bcc30
wmthiet90 <nvthiet.khtn08@gmail.com>
2013-12-11 08:17:43 +0700

    temp

--- catalog/view/javascript/common.js
+++ catalog/view/javascript/common.js
@@ -238,7 +238,39 @@ function HashTable(obj)
 	/*
 		End Notification
 	*/
+	/*
+		Start Mention (Tag)
+	*/
+	function Tag(el) {
+		this.$tagElement = el.find('.mention');
+		this.attachEvents();
+	}
+	Tag.prototype.attachEvents = function() {
+		var that = this;
+		that.$tagElement.mentionsInput({
+			onDataRequest:function (mode, query, callback) {
+			  var data = [
+				{ id:1, name:'Kenneth Auchenberg', 'avatar':'http://cdn0.4dots.com/i/customavatars/avatar7112_1.gif', 'type':'contact' },
+				{ id:2, name:'Jon Froda', 'avatar':'http://cdn0.4dots.com/i/customavatars/avatar7112_1.gif', 'type':'contact' },
+				{ id:3, name:'Anders Pollas', 'avatar':'http://cdn0.4dots.com/i/customavatars/avatar7112_1.gif', 'type':'contact' },
+				{ id:4, name:'Kasper Hulthin', 'avatar':'http://cdn0.4dots.com/i/customavatars/avatar7112_1.gif', 'type':'contact' },
+				{ id:5, name:'Andreas Haugstrup', 'avatar':'http://cdn0.4dots.com/i/customavatars/avatar7112_1.gif', 'type':'contact' },
+				{ id:6, name:'Pete Lacey', 'avatar':'http://cdn0.4dots.com/i/customavatars/avatar7112_1.gif', 'type':'contact' },
+				{ id:7, name:'kenneth@auchenberg.dk', 'avatar':'http://cdn0.4dots.com/i/customavatars/avatar7112_1.gif', 'type':'contact' },
+				{ id:8, name:'Pete Awesome Lacey', 'avatar':'http://cdn0.4dots.com/i/customavatars/avatar7112_1.gif', 'type':'contact' },
+				{ id:9, name:'Kenneth Hulthin', 'avatar':'http://cdn0.4dots.com/i/customavatars/avatar7112_1.gif', 'type':'contact' }
+			  ];
 
+			  data = _.filter(data, function(item) { return item.name.toLowerCase().indexOf(query.toLowerCase()) > -1 });
+			  callback.call(this, data);
+			},
+			minChars: 1,
+			showAvatars: true
+	  	});
+	}
+	/*
+		End Mention (Tag)
+	*/
 	/*
 	Jquery effects
 	*/
@@ -703,6 +735,7 @@ function HashTable(obj)
 		new HorizontalBlock($('.has-horizontal'));
 		new FlexibleElement($(this));
 		new Sidebar($(this));
+		new Tag($(this));
 		if($('#user-notification').length > 0){
 			new Notification($('#user-notification'));
 		}		
