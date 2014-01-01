22b94f49ec0ff5b15d9f55e439daeb2503cf008a
wmthiet90 <nvthiet.khtn08@gmail.com>
2014-01-02 01:26:57 +0700

    update upload file

--- catalog/view/javascript/libs/upload/upload-app.js
+++ catalog/view/javascript/libs/upload/upload-app.js
@@ -76,8 +76,10 @@
                     var link = $('<a>')
                         .prop('href', file.url)
                         .addClass('img-link-popup')
-                        .magnificPopup({type:'image'});           
-                    $(data.context[index]).children('canvas').css('opacity','1').wrap(link);
+                        .magnificPopup({type:'image'}); 
+                    var imageUploaded = $('<img>').prop('src', file.url).addClass('img-uploaded');
+                    $(data.context[index]).children('canvas').replaceWith(imageUploaded);
+                    $(data.context[index]).children('.img-uploaded').wrap(link);
                     if(that.listImgUrl.length == 0){
                         that.listImgUrl = file.url;
                     }else {                    
