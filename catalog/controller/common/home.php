2a9c1b994bc33d88853687c4ce5cf94803e1d68c
bommer <bommer@bommerdesign.com>
2014-01-09 00:21:53 +0700

    - Show image default for post of branch when post's image is empty
    Signed-off-by: bommer <bommer@bommerdesign.com>

--- catalog/controller/common/home.php
+++ catalog/controller/common/home.php
@@ -48,7 +48,7 @@ class ControllerCommonHome extends Controller {
 				if ( isset($aPost['thumb']) && !empty($aPost['thumb']) ){
 					$aPost['image'] = $this->model_tool_image->resize( $aPost['thumb'], 400, 250 );
 				}else{
-					$aPost['image'] = null;
+					$aPost['image'] = $this->model_tool_image->resize( $this->config->get('no_image')['branch']['post'], 400, 250 );
 				}
 
 				$this->data['all_posts'][$sBranchSlug][] = $aPost;
