d66bdefad374c0ba6ec4d90d2edd675442dd36a0
wmthiet90 <nvthiet.khtn08@gmail.com>
2014-02-06 23:46:58 +0700

    init my-branch files

---
+++ catalog/controller/account/branch/category.php
@@ -0,0 +1,26 @@
+<?php
+class ControllerAccountBranchCategory extends Controller {
+	public function index() {
+  		if (isset($this->request->server['HTTPS']) && (($this->request->server['HTTPS'] == 'on') || ($this->request->server['HTTPS'] == '1'))) {
+			$this->data['base'] = $this->config->get('config_ssl');
+		} else {
+			$this->data['base'] = HTTP_SERVER;
+		}
+
+    	$this->document->setTitle($this->language->get('heading_title'));
+
+		if (file_exists(DIR_TEMPLATE . $this->config->get('config_template') . '/template/account/branch/category.tpl')) {
+			$this->template = $this->config->get('config_template') . '/template/account/branch/category.tpl';
+		} else {
+			$this->template = 'default/template/account/branch/category.tpl';
+		}
+		
+		$this->children = array(
+			'common/sidebar_control',
+			'common/footer',
+			'common/header'
+		);
+				
+		$this->response->setOutput($this->twig_render());			
+	}
+}
---
+++ catalog/view/theme/default/template/account/branch/detail.tpl
@@ -0,0 +1,15 @@
+{% extends '@template/default/template/common/layout.tpl' %}
+
+{% use '@template/default/template/common/html_block.tpl' %}
+
+{% block title %}{% trans %}Branch Detail Page{% endtrans %}{% endblock %}
+
+{% block body %}
+    <div id="y-content">
+        <div id="y-main-content" class="has-horizontal" style="width: 9999px;">         
+        </div>
+    </div>
+{% endblock %}
+
+{% block javascript %}
+{% endblock %}
