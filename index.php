902949df29c2b41489a3fa5727c74fb2a0092eb9
Bommer <quangthi_90@yahoo.com.vn>
2014-03-05 20:01:23 +0700

    - Remove language sql code
    Signed-off-by: Bommer <quangthi_90@yahoo.com.vn>

--- index.php
+++ index.php
@@ -4,12 +4,6 @@ define('VERSION', '1.5.3');
 
 // Configuration
 require_once('config.php');
-   
-// Install 
-if (!defined('DIR_APPLICATION')) {
-	header('Location: install/index.php');
-	exit;
-}
 
 // Startup
 require_once(DIR_SYSTEM . 'startup.php');
@@ -97,6 +91,11 @@ setlocale(LC_ALL, $lang);
 bindtextdomain($lang, DIR_LANGUAGE . "locale");
 textdomain($lang);
 
+// Language	
+$language = new Language('english');
+$language->load('english');	
+$registry->set('language', $language); 
+
 // Url
 $url = new Url($config->get('config_url'), $config->get('config_use_ssl') ? $config->get('config_ssl') : $config->get('config_url'));	
 $registry->set('url', $url);
@@ -145,68 +144,14 @@ $response = new Response();
 $response->addHeader('Content-Type: text/html; charset=utf-8');
 $response->setCompression($config->get('config_compression'));
 $registry->set('response', $response); 
-		
+
 // Cache
 $cache = new Cache();
-$registry->set('cache', $cache); 
+$registry->set('cache', $cache);
 
 // Session
 $session = new Session();
 $registry->set('session', $session); 
-
-// Language Detection
-$languages = array();
-
-$db = new DB(DB_DRIVER, DB_HOSTNAME, DB_USERNAME, DB_PASSWORD, DB_DATABASE);
-$query = $db->query("SELECT * FROM " . DB_PREFIX . "language WHERE status = '1'"); 
-
-foreach ($query->rows as $result) {
-	$languages[$result['code']] = $result;
-}
-
-$detect = '';
-
-if (isset($request->server['HTTP_ACCEPT_LANGUAGE']) && ($request->server['HTTP_ACCEPT_LANGUAGE'])) { 
-	$browser_languages = explode(',', $request->server['HTTP_ACCEPT_LANGUAGE']);
-	
-	foreach ($browser_languages as $browser_language) {
-		foreach ($languages as $key => $value) {
-			if ($value['status']) {
-				$locale = explode(',', $value['locale']);
-
-				if (in_array($browser_language, $locale)) {
-					$detect = $key;
-				}
-			}
-		}
-	}
-}
-
-if (isset($session->data['language']) && array_key_exists($session->data['language'], $languages) && $languages[$session->data['language']]['status']) {
-	$code = $session->data['language'];
-} elseif (isset($request->cookie['language']) && array_key_exists($request->cookie['language'], $languages) && $languages[$request->cookie['language']]['status']) {
-	$code = $request->cookie['language'];
-} elseif ($detect) {
-	$code = $detect;
-} else {
-	$code = "en";
-}
-
-if (!isset($session->data['language']) || $session->data['language'] != $code) {
-	$session->data['language'] = $code;
-}
-
-// if (!isset($request->cookie['language']) || $request->cookie['language'] != $code) {	  
-// 	setcookie('language', $code, time() + 60 * 60 * 24 * 30, '/', $request->server['HTTP_HOST']);
-// }
-
-$config->set('config_language_id', $languages[$code]['language_id']);
-$config->set('config_language', $languages[$code]['code']);
-
-// Language	
-$language = new Language($languages[$code]['directory']);
-$language->load($languages[$code]['filename']);	
-$registry->set('language', $language); 
 
 // Document
 $registry->set('document', new Document()); 
