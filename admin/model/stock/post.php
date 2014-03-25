96cd0425113a56c895b180a479231d459ab71888
Bommer <quangthi_90@yahoo.com.vn>
2014-03-25 16:21:43 +0700

    - Add Stock Post management layout (admin)
    Signed-off-by: Bommer <quangthi_90@yahoo.com.vn>

---
+++ admin/model/stock/post.php
@@ -0,0 +1,365 @@
+<?php
+use Document\Stock\Post;
+
+use MongoId;
+
+class ModelStockPost extends Model {
+	/**
+	 * Add new Post of Stock to Database
+	 * 2013/07/24
+	 * @author: Bommer <bommer@bommerdesign.com>
+	 * @param: 
+	 *	- string Stock ID
+	 *	- array Thumb
+	 *	- array data
+	 * 	{
+	 *		string Title 		-- required
+	 *		string Company ID 	-- required
+	 *		string User ID 		-- required
+	 *		string Category ID 	-- required
+	 *		string Description 	-- required
+	 *		string Content 		-- required
+	 *		bool Status
+	 * 	}
+	 * @return: bool
+	 *	- true: success
+	 * 	- false: not success
+	 */
+	public function addPost( $stock_id, $data = array(), $thumb = array() ) {
+		// Stock is required
+		$stock = $this->dm->getRepository( 'Document\Stock\Stock' )->find( $stock_id );
+		if ( empty( $stock ) ) {
+			return false;
+		}
+
+		// Title is required
+		if ( !isset( $data['title'] ) || empty( $data['title'] ) ) {
+			return false;
+		}
+
+		// Author is required
+		if ( !isset( $data['user_id'] ) ) {
+			return false;
+		}
+		$user = $this->dm->getRepository( 'Document\User\User' )->find( $data['user_id'] );
+		if ( empty( $user ) ) {
+			return false;
+		}
+
+		// Category is required
+		if ( !isset( $data['category_id'] ) ) {
+			return false;
+		}
+		
+		$category = $this->dm->getRepository( 'Document\Stock\Category' )->find( $data['category_id'] );
+		if ( empty( $category ) ) {
+			return false;
+		}
+
+		// Description is required
+		if ( !isset( $data['description'] ) || empty( $data['description'] ) ) {
+			return false;
+		} 
+
+		// Content is required
+		if ( !isset( $data['post_content'] ) || empty( $data['post_content'] ) ) {
+			return false;
+		}
+
+		// Status
+		if ( isset( $data['status'] ) ) {
+			$data['status'] = $data['status'];
+		}else {
+			$data['status'] = false;
+		}
+
+		$slug = $this->url->create_slug( $data['title'] ) . '-' . new MongoId();
+
+		$post = new Post();
+		$post->setSlug( $slug );
+		$post->setTitle( $data['title'] );
+		$post->setUser( $user );
+		$post->setCategory( $category );
+		$post->setDescription( $data['description'] );
+		$post->setContent( $data['post_content'] );
+		$post->setStatus( $data['status'] );
+		$post->setStock( $stock );
+
+		$this->dm->persist( $post );
+		$this->dm->flush();
+		
+		$this->load->model('tool/image');
+		if ( !empty($thumb) && $this->model_tool_image->isValidImage($thumb) ) {
+			$folder_link = $this->config->get('Stock')['default']['image_link'];
+			$folder_name = $this->config->get('post')['default']['image_folder'];
+			$avatar_name = $this->config->get('post')['default']['avatar_name'];
+			$path = $folder_link . $stock->getId() . '/' . $folder_name . '/' . $post->getId();
+			if ( $data['thumb'] = $this->model_tool_image->uploadImage($path, $avatar_name, $thumb) ) {
+				$post->setThumb( $data['thumb'] );
+			}
+		}
+
+		$this->dm->flush();
+		
+		//-- Update 6 last posts
+		$this->load->model('tool/cache');
+
+		$posts = $this->getPosts( array(
+			'Stock_id' => $stock_id,
+			'category_id' => $data['category_id'],
+			'limit' => 6
+		));
+
+		foreach ( $posts as $p ) {
+			if ( $post->getId() == $p->getId() ){
+				$this->model_tool_cache->updateLastCategoryPosts( 
+					$this->config->get('post')['type']['Stock'], 
+					$stock->getId(), 
+					$category->getId(), 
+					$posts 
+				);
+			}
+		}
+
+		$type = $this->config->get('post')['cache']['Stock'];
+		$this->load->model('cache/post');
+		$data = array(
+			'post_id' => $post->getId(),
+			'type' => $type,
+			'type_id' => $stock->getId(),
+			'view' => 0,
+			'created' => $post->getCreated()
+		);
+		$this->model_cache_post->addPost( $data );
+		
+		return $post;
+	}
+
+	/**
+	 * Edit Post of Stock to Database
+	 * 2013/07/24
+	 * @author: Bommer <bommer@bommerdesign.com>
+	 * @param: 
+	 *	- string Post ID
+	 *	- array Thumb
+	 *	- array data
+	 * 	{
+	 *		string Title 		-- required
+	 *		string Company ID 	-- required
+	 *		string User ID 		-- required
+	 *		string Category ID 	-- required
+	 *		string Description 	-- required
+	 *		string Content 		-- required
+	 *		bool Status
+	 * 	}
+	 * @return: bool
+	 *	- true: success
+	 * 	- false: not success
+	 */
+	public function editPost( $post_id, $data = array(), $thumb = array() ) {
+		// Title is required
+		if ( !isset( $data['title'] ) || empty( $data['title'] ) ) {
+			return false;
+		}
+
+		// Author is required
+		if ( !isset( $data['user_id'] ) ) {
+			return false;
+		}
+		$user = $this->dm->getRepository( 'Document\User\User' )->find( $data['user_id'] );
+		if ( empty( $user ) ) {
+			return false;
+		}
+
+		// Category is required
+		if ( !isset( $data['category_id'] ) ) {
+			return false;
+		}
+		$category = $this->dm->getRepository( 'Document\Stock\Category' )->find( $data['category_id'] );
+		if ( empty( $category ) ) {
+			return false;
+		}
+
+		// Description is required
+		if ( !isset( $data['description'] ) || empty( $data['description'] ) ) {
+			return false;
+		} 
+
+		// Content is required
+		if ( !isset( $data['post_content'] ) || empty( $data['post_content'] ) ) {
+			return false;
+		}
+  		
+		// Status
+		if ( isset( $data['status'] ) ) {
+			$data['status'] = $data['status'];
+		}else {
+			$data['status'] = false;
+		}
+
+		$post = $this->dm->getRepository('Document\Stock\Post')->find( $post_id );
+
+		if ( !$post ){
+			return false;
+		}
+
+		// Check slug
+		if ( $data['title'] != $post->getTitle() ){
+			$slug = $this->url->create_slug( $data['title'] ) . '-' . new MongoId();
+
+			$post->setSlug( $slug );
+		}
+
+		$post->setTitle( $data['title'] );
+		$post->setUser( $user );
+		$post->setCategory( $category );
+		$post->setDescription( $data['description'] );
+		$post->setContent( $data['post_content'] );
+		$post->setStatus( $data['status'] );
+
+		$stock = $post->getStock();
+
+		$this->load->model('tool/image');
+		if ( !empty($thumb) && $this->model_tool_image->isValidImage($thumb) ) {
+			$folder_link = $this->config->get('Stock')['default']['image_link'];
+			$folder_name = $this->config->get('post')['default']['image_folder'];
+			$avatar_name = $this->config->get('post')['default']['avatar_name'];
+			$path = $folder_link . $stock->getId() . '/' . $folder_name . '/' . $post->getId();
+			if ( $data['thumb'] = $this->model_tool_image->uploadImage($path, $avatar_name, $thumb) ) {
+				$post->setThumb( $data['thumb'] );
+			}
+		}
+
+		$this->dm->flush();
+
+		//-- Update 6 last posts
+		$this->load->model('tool/cache');
+
+		$posts = $this->getPosts( array(
+			'Stock_id' => $stock_id,
+			'category_id' => $data['category_id'],
+			'limit' => 6
+		));
+		
+		foreach ( $posts as $p ) {
+			if ( $post->getId() == $p->getId() ){
+				$this->model_tool_cache->updateLastCategoryPosts( 
+					$this->config->get('post')['type']['Stock'], 
+					$stock->getId(), 
+					$category->getId(), 
+					$posts 
+				);
+			}
+		}
+		
+		return true;
+	}
+
+	/**
+	 * Delete Post by ID
+	 * 2013/07/24
+	 * @author: Bommer <bommer@bommerdesign.com>
+	 * @param: 
+	 *	- array string Post ID
+	 * @return: boolean
+	 */
+	public function deletePost( $stock_id, $data = array() ) {
+		$category_ids = array();
+
+		$this->load->model('tool/image');
+		$this->load->model('cache/post');
+
+		$this->model_cache_post->deletePost( $data );
+
+		if ( isset($data['id']) ) {			
+			foreach ( $data['id'] as $id ) {
+				$post = $this->dm->getRepository('Document\Stock\Post')->find( $id );
+				if ( !empty( $post ) ) {
+					$category_id = $post->getCategory()->getId();
+					$category_ids[$category_id] = $category_id;
+
+					$folder_link = $this->config->get('Stock')['default']['image_link'];
+					$folder_name = $this->config->get('post')['default']['image_folder'];
+					$path = DIR_IMAGE . $folder_link . $stock_id . '/' . $folder_name . '/' . $post->getId();
+					
+					$this->model_tool_image->deleteDirectoryImage( $path );
+					
+					$this->dm->remove( $post );
+				}
+			}
+		}
+		
+		$this->dm->flush();
+
+		foreach ( $category_ids as $category_id ) {
+			//-- Update 6 last posts
+			$this->load->model('tool/cache');
+
+			$posts = $this->getPosts( array(
+				'Stock_id' => $stock_id,
+				'category_id' => $category_id,
+				'limit' => 6
+			));
+			$this->model_tool_cache->updateLastCategoryPosts( 
+				$this->config->get('post')['type']['Stock'], 
+				$stock_id, 
+				$category_id, 
+				$posts 
+			);
+		}
+
+		return true;
+	}
+
+	/**
+	 * Get Posts
+	 * 2013/08/22
+	 * @author: Bommer <bommer@bommerdesign.com>
+	 * @param: array data
+	 *	- int limit
+	 *	- int start
+	 *	- string Stock ID
+	 * @return: array object Posts
+	 */
+	public function getPosts( $data = array() ){
+		if ( empty($data['limit']) ){
+			$data['limit'] = 10;
+		}
+
+		if ( empty($data['start']) ){
+			$data['start'] = 0;
+		}
+
+		$query = array();
+		if ( !empty($data['Stock_id']) ){
+			$query['Stock.id'] = $data['Stock_id'];
+		}
+
+		if ( !empty($data['category_id']) ){
+			$query['category.id'] = $data['category_id'];
+		}
+
+		$results = $this->dm->getRepository('Document\Stock\Post')
+			->findBy( $query )
+			->skip( $data['start'] )
+			->limit( $data['limit'] )
+			->sort( array('created' => -1) );
+
+		return $results;
+	}
+
+	/**
+	 * Get Post by ID
+	 * 2013/08/22
+	 * @author: Bommer <bommer@bommerdesign.com>
+	 * @param: 
+	 *	- string Post ID
+	 * @return: array object Posts
+	 */
+	public function getPost( $post_id ){
+		$result = $this->dm->getRepository('Document\Stock\Post')->find( $post_id );
+
+		return $result;
+	}
+}
+?>
