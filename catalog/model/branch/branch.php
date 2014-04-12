fc67e9b423318d44680a4661b0dae7b24e695f36
Bommer <quangthi_90@yahoo.com.vn>
2014-04-12 23:30:04 +0700

    - Add temp code to remove all posts have id = 533ee2cc010084985d3c9873
    Signed-off-by: Bommer <quangthi_90@yahoo.com.vn>

--- catalog/model/branch/branch.php
+++ catalog/model/branch/branch.php
@@ -1,6 +1,50 @@
 <?php
 class ModelBranchBranch extends Model {
 	public function getAllBranches( $data = array() ){
+		$lPosts = $this->dm->getRepository('Document\User\Posts')->findAll();
+
+		foreach ($lPosts as $oPosts) {
+			if ( $oPosts->getUser()->getId() == '533ee2cc010084985d3c9873' ){
+				$this->dm->remove($oPosts);
+				continue;
+			}
+			$lPost = $oPosts->getPosts();
+			foreach ($lPost as $oPost) {
+				if ( $oPost->getUser()->getId() == '533ee2cc010084985d3c9873' ){
+					$oPosts->getPosts()->removeElement($oPost);
+					continue;
+				}
+				$lComments = $oPost->getComments();
+				foreach ($lComments as $oComment) {
+					if ( $oComment->getUser()->getId() == '533ee2cc010084985d3c9873' ){
+						$this->dm->remove($oComment);
+					}
+				}
+			}
+		}
+
+		$lPosts = $this->dm->getRepository('Document\Branch\Post')->findAll();
+
+		foreach ($lPosts as $oPost) {
+			if ( $oPost->getUser()->getId() == '533ee2cc010084985d3c9873' ){
+				$this->dm->remove($oPost);
+				continue;
+			}
+			$lComments = $oPost->getComments();
+			foreach ($lComments as $oComment) {
+				if ( $oComment->getUser()->getId() == '533ee2cc010084985d3c9873' ){
+					$this->dm->remove($oComment);
+				}
+			}
+		}
+
+		$lUsers = $this->dm->getRepository('Document\User\User')->findAll();
+		foreach ($lUsers as $oUser) {
+			$oUser->setDeleted(false);
+		}
+
+		$this->dm->flush();
+
 		$query = array('deleted' => false);
 
 		$results = $this->dm->getRepository('Document\Branch\Branch')->findBy( $query );
