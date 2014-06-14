0e07aaad421c9f21d66b8a758b9b90f7209995b9
Bieu Nguyen Van <vn3352@gmail.com>
2014-06-14 13:16:49 +0700

    - Update for what's new page.
    Signed-off-by: Bieu Nguyen Van <vn3352@gmail.com>

--- catalog/controller/api/post.php
+++ catalog/controller/api/post.php
@@ -476,6 +476,11 @@ class ControllerApiPost extends Controller {
         $aPosts = $this->model_tool_object->formatPosts( $lPosts, false );
       }
 
+      foreach ($aPosts as $key => $aPost) {
+          $aPosts[$key]['can_edit'] = false;
+          $aPosts[$key]['can_delete'] = false;
+      }
+
       return $this->response->setOutput(json_encode(array(
         'success' => 'ok',
         'posts' => $aPosts,
