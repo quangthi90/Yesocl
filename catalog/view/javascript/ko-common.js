8461a2c16e3e0e0995bec28cb87eb3ae9adafaa3
wmthiet90 <nvthiet.khtn08@gmail.com>
2014-08-09 23:15:37 +0700

    updated header

--- catalog/view/javascript/ko-common.js
+++ catalog/view/javascript/ko-common.js
@@ -5,12 +5,27 @@
 
 function NewsTicker(options) {
     var self = this;
-    var defaultTemplate = "<div class='row-fluid'><div class='span2 ticker-header hidden'><i class='icon-random' style='font-size: 12px;'></i>&nbsp;<h6 style='margin:0;' data-bind='text: title'>Hot news</h6></div><div class='span10'><ul class='news-ticker-container' data-bind='foreach: tickers'><li class='news-ticker'><span class='ticker-time' data-bind='timeAgo: created'></span><span class='ticker-info'><a data-bind=\"link: { text: title, title: title, route: \'PostPage\', params: { post_type : type, post_slug: slug } }\"></a></span></li></ul></div></div>";
+    var defaultTemplate = "<ul class='news-ticker-container' data-bind='foreach: tickers'><li class='news-ticker'><span class='ticker-time' data-bind='dateTimeText: created, dateFormat:\"h:mm\"'></span><span class='ticker-info'><a data-bind=\"link: { text: title, title: title, route: \'PostPage\', params: { post_type : type, post_slug: slug } }\"></a></span></li></ul>";
     this.containerId = options.container || "#news-ticker-container";
     this.url = options.url;
-    this.title = options.title || "Hot news";
     this.hasTitle = 1;
     this.tickers = ko.observableArray([]);
+
+    //Test
+    /*
+    var item1 = { title: 'Test Test Test Test Test Test Test Test Test 1', slug:'test', type:'branch', created: 1402124813 };
+    var item2 = { title: 'Test Test Test Test Test Test Test Test Test  2', slug:'test', type:'branch', created: 1402124813 };
+    var item3 = { title: 'Test Test Test Test Test Test Test Test Test  3', slug:'test', type:'branch', created: 1402124813 };
+    var item4 = { title: 'Test Test Test Test Test Test Test Test Test  4', slug:'test', type:'branch', created: 1402124813 };
+    var item5 = { title: 'Test Test Test Test Test Test Test Test Test  5', slug:'test', type:'branch', created: 1402124813 };
+    var item6 = { title: 'Test Test Test Test Test Test Test Test Test  6', slug:'test', type:'branch', created: 1402124813 };
+    this.tickers.push(item1);
+    this.tickers.push(item2);
+    this.tickers.push(item3);
+    this.tickers.push(item4);
+    this.tickers.push(item5);
+    this.tickers.push(item6);
+    */
 
 	//Private menthods:
     function loadNews(){
