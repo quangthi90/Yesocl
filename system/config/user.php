6a79315ff9ab35f581f9a58b8c07885fcaf7c75c
tuyetphale993 <tuyetphale993@gmail.com>
2014-09-16 16:51:18 +0700

    - add url cover
    Signed-off-by: tuyetphale993 <tuyetphale993@gmail.com>

--- system/config/user.php
+++ system/config/user.php
@@ -3,6 +3,8 @@
 $_['user']['default']['group_name'] = 'Default';
 $_['user']['default']['image_link'] = 'data/catalog/user/';
 $_['user']['default']['cache_link'] = 'user/';
+$_['user']['default']['image_link'] = 'data/catalog/user/';
+$_['user']['default']['cache_link'] = 'user/';
 
 $_['user']['network']['default'] = 'default';
 $_['user']['network']['facebook'] = 'facebook';
