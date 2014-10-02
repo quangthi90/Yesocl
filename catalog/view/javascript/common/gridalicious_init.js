25329339eb6f200a928f144db38300b4df938205
wmthiet90 <nvthiet.khtn08@gmail.com>
2014-10-03 00:48:02 +0700

    Updated

--- catalog/view/javascript/common/gridalicious_init.js
+++ catalog/view/javascript/common/gridalicious_init.js
@@ -2,13 +2,17 @@
 	$(window).on('gridalicious-loaded', function(){
 		$('[data-toggle*="gridalicious"]').each(function() {
 			var $that = $(this);
+			var gutter = $that.attr('data-gridalicious-gutter') ? parseInt($that.attr('data-gridalicious-gutter')) : 13;
+			var width = $that.attr('data-gridalicious-width') ? parseInt($that.attr('data-gridalicious-width')) : 200;
+			var	selector = $that.attr('data-gridalicious-selector') || '.widget';
+
 			$(this).find('.loading').remove().end()
 			.find('.loaded').removeClass('hide2').end()
 			.gridalicious(
 			{
-				gutter: $that.attr('data-gridalicious-gutter') ? parseInt($that.attr('data-gridalicious-gutter')) : 13, 
-				width: $that.attr('data-gridalicious-width') ? parseInt($that.attr('data-gridalicious-width')) : 200,
-				selector: $that.attr('data-gridalicious-selector') || '.widget'
+				gutter: gutter, 
+				width: width,
+				selector: selector
 			});
 		});
 	});
