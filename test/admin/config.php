19a127234f96bdff7a7c0b318d1d084c056d64f6
Bommer <quangthi_90@yahoo.com.vn>
2014-10-30 12:18:31 +0700

    - Update config file for VPS
    Signed-off-by: Bommer <quangthi_90@yahoo.com.vn>

--- test/admin/config.php
+++ test/admin/config.php
@@ -1,34 +1,34 @@
 <?php
 // HTTP
-define('HTTP_SERVER', 'http://localhost/yesocl/admin/');
-define('HTTP_CATALOG', 'http://localhost/yesocl/');
-define('HTTP_IMAGE', 'http://localhost/yesocl/image/');
+define('HTTP_SERVER', 'http://yesocl.com/test/admin/');
+define('HTTP_IMAGE', 'http://yesocl.com/test/image/');
+define('HTTP_CATALOG', 'http://yesocl.com/test/');
 
 // HTTPS
-define('HTTPS_SERVER', 'http://localhost/yesocl/admin/');
-define('HTTPS_CATALOG', 'http://localhost/yesocl/');
-define('HTTPS_IMAGE', 'http://localhost/yesocl/image/');
+define('HTTPS_SERVER', 'http://yesocl.com/test/admin');
+define('HTTPS_IMAGE', 'http://yesocl.com/test/image/');
+define('HTTPS_CATALOG', 'http://yesocl.com/test/');
 
 // DIR
-define('DIR_APPLICATION', 'F:\Program\xampp\htdocs\Yesocl/admin/');
-define('DIR_SYSTEM', 'F:\Program\xampp\htdocs\Yesocl/system/');
-define('DIR_DATABASE', 'F:\Program\xampp\htdocs\Yesocl/system/database/');
-define('DIR_LANGUAGE', 'F:\Program\xampp\htdocs\Yesocl/admin/language/');
-define('DIR_TEMPLATE', 'F:\Program\xampp\htdocs\Yesocl/admin/view/template/');
-define('DIR_CONFIG', 'F:\Program\xampp\htdocs\Yesocl/system/config/');
-define('DIR_IMAGE', 'F:\Program\xampp\htdocs\Yesocl/image/');
-define('DIR_CACHE', 'F:\Program\xampp\htdocs\Yesocl/system/cache/');
-define('DIR_DOWNLOAD', 'F:\Program\xampp\htdocs\Yesocl/download/');
-define('DIR_LOGS', 'F:\Program\xampp\htdocs\Yesocl/system/logs/');
-define('DIR_CATALOG', 'F:\Program\xampp\htdocs\Yesocl/catalog/');
+define('DIR_APPLICATION', '/home/admin/public_html/test/admin/');
+define('DIR_SYSTEM', '/home/admin/public_html/test/system/');
+define('DIR_DATABASE', '/home/admin/public_html/test/system/database/');
+define('DIR_LANGUAGE', '/home/admin/public_html/test/admin/language/');
+define('DIR_TEMPLATE', '/home/admin/public_html/test/admin/view/template/');
+define('DIR_CONFIG', '/home/admin/public_html/test/system/config/');
+define('DIR_IMAGE', '/home/admin/public_html/test/image/');
+define('DIR_CACHE', '/home/admin/public_html/test/system/cache/');
+define('DIR_DOWNLOAD', '/home/admin/public_html/test/download/');
+define('DIR_LOGS', '/home/admin/public_html/test/system/logs/');
+define('DIR_CATALOG', '/home/admin/public_html/test/catalog/');
 
-define('DIR_ROOT', 'F:\Program\xampp\htdocs\Yesocl/');
+define('DIR_ROOT', '/home/admin/public_html/test/');
 
 // DB
 define('DB_DRIVER', 'mysql');
 define('DB_HOSTNAME', 'localhost');
-define('DB_USERNAME', 'root');
-define('DB_PASSWORD', '');
+define('DB_USERNAME', 'yesocl');
+define('DB_PASSWORD', 'yesocl@12345@abc!@#');
 define('DB_DATABASE', 'yesocl');
 define('DB_PREFIX', '');
 ?>
--- test/config.php
+++ test/config.php
@@ -1,43 +1,37 @@
 <?php
 // HTTP
-define('HTTP_SERVER', 'http://localhost/yesocl/test/');
-define('HTTP_CATALOG', 'http://localhost/yesocl/test/');
-define('HTTP_IMAGE', 'http://localhost/yesocl/test/image/');
+define('HTTP_SERVER', 'http://yesocl.com/test/');
+define('HTTP_IMAGE', 'http://yesocl.com/test/image/');
+define('HTTP_ADMIN', 'http://yesocl.com/test/admin/');
 
 // HTTPS
-define('HTTPS_SERVER', 'http://localhost/yesocl/test/');
-define('HTTPS_CATALOG', 'http://localhost/yesocl/test/');
-define('HTTPS_IMAGE', 'http://localhost/yesocl/test/image/');
+define('HTTPS_SERVER', 'http://yesocl.com/test/');
+define('HTTPS_IMAGE', 'http://yesocl.com/test/image/');
 
-// DIR
-define('DIR_APPLICATION', 'F:\Program\xampp\htdocs\Yesocl/test/catalog/');
-define('DIR_SYSTEM', 'F:\Program\xampp\htdocs\Yesocl/test/system/');
-define('DIR_DATABASE', 'F:\Program\xampp\htdocs\Yesocl/test/system/database/');
-define('DIR_LANGUAGE', 'F:\Program\xampp\htdocs\Yesocl/test/catalog/language/');
-define('DIR_TEMPLATE', 'F:\Program\xampp\htdocs\Yesocl/test/catalog/view/theme/');
-define('DIR_CONFIG', 'F:\Program\xampp\htdocs\Yesocl/test/system/config/');
-define('DIR_IMAGE', 'F:\Program\xampp\htdocs\Yesocl/test/image/');
-define('DIR_CACHE', 'F:\Program\xampp\htdocs\Yesocl/test/system/cache/');
-define('DIR_DOWNLOAD', 'F:\Program\xampp\htdocs\Yesocl/test/download/');
-define('DIR_LOGS', 'F:\Program\xampp\htdocs\Yesocl/test/system/logs/');
 
-define('DIR_ROOT', 'F:\Program\xampp\htdocs\Yesocl/test/');
-define('YS_VERSION', 1.1);
+define('DIR_APPLICATION', '/home/admin/public_html/test/catalog/');
+define('DIR_SYSTEM', '/home/admin/public_html/test/system/');
+define('DIR_DATABASE', '/home/admin/public_html/test/system/database/');
+define('DIR_LANGUAGE', '/home/admin/public_html/test/catalog/language/');
+define('DIR_TEMPLATE', '/home/admin/public_html/test/catalog/view/theme/');
+define('DIR_CONFIG', '/home/admin/public_html/test/system/config/');
+define('DIR_IMAGE', '/home/admin/public_html/test/image/');
+define('DIR_CACHE', '/home/admin/public_html/test/system/cache/');
+define('DIR_DOWNLOAD', '/home/admin/public_html/test/download/');
+define('DIR_LOGS', '/home/admin/public_html/test/system/logs/');
+
+define('DIR_ROOT', '/home/admin/public_html/test/');
+define('YS_VERSION', '1.5.1');
 
 // Facebook
-define('FB_API_ID', '588076974594904');
-define('FB_API_SECRET', '40b7350fd7c011a9dab59d9f22722679');
-
-// Pusher
-define('PUSHER_APP_KEY', '22dc8822a1badda84d02');
-define('PUSHER_APP_SECRET', 'f27ab33b3ec6453e693d');
-define('PUSHER_APP_ID', '88643');
+define('FB_API_ID', '562278927194046');
+define('FB_API_SECRET', '2290a3072f7d664613cfbdc14fab632d');
 
 // DB
 define('DB_DRIVER', 'mysql');
 define('DB_HOSTNAME', 'localhost');
-define('DB_USERNAME', 'root');
-define('DB_PASSWORD', '');
+define('DB_USERNAME', 'yesocl');
+define('DB_PASSWORD', 'yesocl@12345@abc!@#');
 define('DB_DATABASE', 'yesocl');
 define('DB_PREFIX', '');
 ?>
