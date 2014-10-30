946dad66d96fef91171df64d54d66d75f5415c06
Bommer <quangthi_90@yahoo.com.vn>
2014-10-30 12:16:49 +0700

    - Add config
    Signed-off-by: Bommer <quangthi_90@yahoo.com.vn>

---
+++ test/config.php
@@ -0,0 +1,43 @@
+<?php
+// HTTP
+define('HTTP_SERVER', 'http://localhost/yesocl/test/');
+define('HTTP_CATALOG', 'http://localhost/yesocl/test/');
+define('HTTP_IMAGE', 'http://localhost/yesocl/test/image/');
+
+// HTTPS
+define('HTTPS_SERVER', 'http://localhost/yesocl/test/');
+define('HTTPS_CATALOG', 'http://localhost/yesocl/test/');
+define('HTTPS_IMAGE', 'http://localhost/yesocl/test/image/');
+
+// DIR
+define('DIR_APPLICATION', 'F:\Program\xampp\htdocs\Yesocl/test/catalog/');
+define('DIR_SYSTEM', 'F:\Program\xampp\htdocs\Yesocl/test/system/');
+define('DIR_DATABASE', 'F:\Program\xampp\htdocs\Yesocl/test/system/database/');
+define('DIR_LANGUAGE', 'F:\Program\xampp\htdocs\Yesocl/test/catalog/language/');
+define('DIR_TEMPLATE', 'F:\Program\xampp\htdocs\Yesocl/test/catalog/view/theme/');
+define('DIR_CONFIG', 'F:\Program\xampp\htdocs\Yesocl/test/system/config/');
+define('DIR_IMAGE', 'F:\Program\xampp\htdocs\Yesocl/test/image/');
+define('DIR_CACHE', 'F:\Program\xampp\htdocs\Yesocl/test/system/cache/');
+define('DIR_DOWNLOAD', 'F:\Program\xampp\htdocs\Yesocl/test/download/');
+define('DIR_LOGS', 'F:\Program\xampp\htdocs\Yesocl/test/system/logs/');
+
+define('DIR_ROOT', 'F:\Program\xampp\htdocs\Yesocl/test/');
+define('YS_VERSION', 1.1);
+
+// Facebook
+define('FB_API_ID', '588076974594904');
+define('FB_API_SECRET', '40b7350fd7c011a9dab59d9f22722679');
+
+// Pusher
+define('PUSHER_APP_KEY', '22dc8822a1badda84d02');
+define('PUSHER_APP_SECRET', 'f27ab33b3ec6453e693d');
+define('PUSHER_APP_ID', '88643');
+
+// DB
+define('DB_DRIVER', 'mysql');
+define('DB_HOSTNAME', 'localhost');
+define('DB_USERNAME', 'root');
+define('DB_PASSWORD', '');
+define('DB_DATABASE', 'yesocl');
+define('DB_PREFIX', '');
+?>
