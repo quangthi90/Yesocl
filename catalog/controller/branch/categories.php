61fb112d7e4af34fd365e1e144e34afb5d7ce7b2
Bommer <quangthi_90@yahoo.com.vn>
2014-12-03 16:34:23 +0700

    - Fix list post's images size
    Signed-off-by: Bommer <quangthi_90@yahoo.com.vn>

--- catalog/controller/branch/categories.php
+++ catalog/controller/branch/categories.php
@@ -46,7 +46,7 @@ class ControllerBranchCategories extends Controller {
 				'slug' => $oCategory->getSlug()
 			);
 			
-			$aPosts = $this->model_tool_object->formatPosts( $lPosts, false );
+			$aPosts = $this->model_tool_object->formatPosts( $lPosts, false, true, 344, 175 );
 
 			$this->data['all_posts'][$oCategory->getId()] = $aPosts;
 		}
